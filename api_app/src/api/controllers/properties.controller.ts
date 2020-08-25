dd2dc7cc1cb86f832b40f35f0abcb5b271f28b6a
Tuan Huynh <tuanhuynht1998@gmail.com>
2020-08-25 11:25:18 -0400

    filled out list-for-sale payload

--- api_app/src/api/controllers/properties.controller.ts
+++ api_app/src/api/controllers/properties.controller.ts
@@ -1,12 +1,11 @@
 import { Response, Request, Router } from "express";
-import { property } from "lodash";
 
 const router: Router = Router();
 const unirest = require("unirest");
 
-router.get("/list-for-rent", async (req: Request, res: Response) => {
+router.get("/list-for-sale", async (req: Request, res: Response) => {
     
-    let realtorReq = unirest("GET", "https://realtor.p.rapidapi.com/properties/v2/list-for-rent");
+    let realtorReq = unirest("GET", "https://realtor.p.rapidapi.com/properties/v2/list-for-sale");
 
     realtorReq.query(req.query);
     // hardcode now but should be from ENV
@@ -18,29 +17,40 @@ router.get("/list-for-rent", async (req: Request, res: Response) => {
 
     try {
         await realtorReq.end( (realtorRes: any) => {        
-            
+
             let properties = realtorRes.body.properties.map((prop : any) :any =>  {
                 return {
                     propertyId: prop.property_id,
                     listingId: prop.listing_id,
-                    price: prop.community, /* change in front-end */ 
-                    // propertyType: getPropertyType(prop.prop_type)
-                    // address: {
-                    //     street1: string;
-                    //     street2: string;
-                    //     city: string;
-                    //     zipcode: string;
-                    //     state: string;
-                    //     lat: number;
-                    //     lng: number;
-                    // }
+                    price: prop.price, 
+                    propertyType: consolidatePropertyType(prop.prop_type),
+                    // list_date: prop.list_date,
+                    last_update: prop.last_update,
+                    // year_built: props.year_built
+                    // listing_status: prop.listing_status,
+                    beds: prop.beds,
+                    baths_full: prop.baths_full,
+                    baths: prop.baths,
+                    prop_status: "for_sale",
+                    photos:  [prop.thumbnail], // just get thumbnail for now, will call another api GET to get the rest
+                    sqfeet: prop.building_size.size, // adjust later to accomadate other units
+                    address: {
+                        line: prop.address.line,
+                        city: prop.address.city,
+                        postal_code: prop.address.postal_code,
+                        state: prop.address.state,
+                        state_code: prop.address.state_code,
+                        neighborhood_name: prop.address.neighborhood_name,
+                        lat: prop.address.lat,
+                        lon: prop.address.lon
+                    }
                 }
             });
             
             console.log(properties);
 
-            res.status(200).json(realtorRes.body.properties);
-            // res.status(200).json(properties);
+            // res.status(200).json(realtorRes.body.properties);
+            res.status(200).json(properties);
         });
     } catch (e) {
         console.log(e);
@@ -48,12 +58,14 @@ router.get("/list-for-rent", async (req: Request, res: Response) => {
     
 });
 
-const getPropertyType = (propertyType: string) : string => {
+const consolidatePropertyType = (propertyType: string) : string => {
 
     if (propertyType === "apartment" || propertyType === "condo") 
         return propertyType
     else if (propertyType === "single_family" || propertyType === "multiple_family") 
         return "house"
+    else 
+        return "other" 
 }
 
 module.exports = router;
