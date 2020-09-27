02965ffeee885ebae53237889cd351115606ac89
Igor Amidzic <amidzicigor46@gmail.com>
2020-09-26 21:14:28 -0400

    Heroku

--- web_app/src/environments/environment.prod.ts
+++ web_app/src/environments/environment.prod.ts
@@ -1,4 +1,5 @@
 export const environment = {
   production: true,
-  google_api_key: 'AIzaSyARSeuHCRLBkzrHS9SLqsl9XSvdE33-sJk'
+  google_api_key: 'AIzaSyARSeuHCRLBkzrHS9SLqsl9XSvdE33-sJk',
+  apiBase: 'https://real-estate-app-1.herokuapp.com/api',
 };
