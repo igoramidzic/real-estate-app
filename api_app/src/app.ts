1edb23cfd4184e4ef9e13fee9153cc036dab66ee
Igor Amidzic <amidzicigor46@gmail.com>
2020-09-26 20:35:01 -0400

    Add heroku requirements

--- api_app/src/app.ts
+++ api_app/src/app.ts
@@ -1,6 +1,7 @@
 import express from "express";
 import cors from "cors";
 import dotenv from "dotenv";
+import path from 'path'
 dotenv.config({ path: ".env" });
 
 // Create Express server
@@ -10,8 +11,12 @@ const app = express();
 app.set("port", process.env.PORT || 3000);
 app.use(express.json());
 app.use(cors());
+app.use(express.static(path.join(__dirname, './../../web_app/dist/web_app')));
 
 // Routes
 app.use("/api", require("./api/controllers/index"));
+
+// Return frontend app
+app.use('**', express.static(path.join(__dirname, './../../web_app/dist/web_app')))
 
 export default app;
